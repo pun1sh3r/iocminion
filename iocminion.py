7e6191de119220191f225dff7d10744c47f719ac
lmendieta <luiguibiker@gmail.com>
2015-07-23 17:53:03 -0400

    fixes issue #1

--- iocminion.py
+++ iocminion.py
@@ -54,10 +54,8 @@ class iocMinion():
     self.domainRegex = "\\b([A-Z0-9_\-\.]+\[?\.\]?(?:XN--CLCHC0EA0B2G2A9GCD|XN--HGBK6AJ7F53BBA|XN--HLCJ6AYA9ESC7A|XN--11B5BS3A9AJ6G|XN--MGBERP4A5D4AR|XN--XKC2DL3A5EE0H|XN--80AKHBYKNJ4F|XN--XKC2AL3HYE2A|XN--LGBBAT1AD8J|XN--MGBC0A9AZCG|XN--9T4B11YI5A|XN--MGBAAM7A8H|XN--MGBAYH7GPA|XN--MGBBH1A71E|XN--FPCRJ9C3D|XN--FZC2C9E2C|XN--YFRO4I67O|XN--YGBI2AMMX|XN--3E0B707E|XN--JXALPDLP|XN--KGBECHTV|XN--OGBPF8FL|XN--0ZWM56D|XN--45BRJ9C|XN--80AO21A|XN--DEBA0AD|XN--G6W251D|XN--GECRJ9C|XN--H2BRJ9C|XN--J6W193G|XN--KPRW13D|XN--KPRY57D|XN--PGBS0DH|XN--S9BRJ9C|XN--90A3AC|XN--FIQS8S|XN--FIQZ9S|XN--O3CW4H|XN--WGBH1C|XN--WGBL6A|XN--ZCKZAH|XN--P1AI|XN--NGBC5AZD|XN--80ASEHDB|XN--80ASWG|XN--UNUP4Y|MUSEUM|TRAVEL|AERO|ARPA|ASIA|COOP|INFO|JOBS|MOBI|BIZ|CAT|COM|EDU|GOV|INT|MIL|NET|ORG|PRO|TEL|XXX|AC|AD|AE|AF|AG|AI|AL|AM|AN|AO|AQ|AR|AS|AT|AU|AW|AX|AZ|BA|BB|BD|BE|BF|BG|BH|BI|BJ|BM|BN|BO|BR|BS|BT|BV|BW|BY|BZ|CA|CC|CD|CF|CG|CH|CI|CK|CL|CM|CN|CO|CR|CU|CV|CW|CX|CY|CZ|DE|DJ|DK|DM|DO|DZ|EC|EE|EG|ER|ES|ET|EU|FI|FJ|FK|FM|FO|FR|GA|GB|GD|GE|GF|GG|GH|GI|GL|GM|GN|GP|GQ|GR|GS|GT|GU|GW|GY|HK|HM|HN|HR|HT|HU|ID|IE|IL|IM|IN|IO|IQ|IR|IS|IT|JE|JM|JO|JP|KE|KG|KH|KI|KM|KN|KP|KR|KW|KY|KZ|LA|LB|LC|LI|LK|LR|LS|LT|LU|LV|LY|MA|MC|MD|ME|MG|MH|MK|ML|MM|MN|MO|MP|MQ|MR|MS|MT|MU|MV|MW|MX|MY|MZ|NA|NC|NE|NF|NG|NI|NL|NO|NP|NR|NU|NZ|OM|PA|PE|PF|PG|PH|PK|PL|PM|PN|PR|PS|PT|PW|PY|QA|RE|RO|RS|RU|RW|SA|SB|SC|SD|SE|SG|SH|SI|SJ|SK|SL|SM|SN|SO|SR|ST|SU|SV|SX|SY|SZ|TC|TD|TF|TG|TH|TJ|TK|TL|TM|TN|TO|TP|TR|TT|TV|TW|TZ|UA|UG|UK|US|UY|UZ|VA|VC|VE|VG|VI|VN|VU|WF|WS|YE|YT|ZA|ZM|ZW))(?:\s|$)\\b"
   
   def sendEmail(self,recipients,body,subject,files=None):
-    print "sendEmail"
     try:
-      #atims relay 10.1.47.116
-      s = smtplib.SMTP('smtp.vzbi.com')
+      s = smtplib.SMTP('relay.smtp.com')
       s.set_debuglevel(1)
       if files:
 	msg = MIMEMultipart()
@@ -68,14 +66,10 @@ class iocMinion():
 	  msg.attach(part)
       else:
 	msg = MIMEText(body)
-      sender = 'autobot@verizon.com'
+      sender = 'sender@sender.com'
       msg['Subject'] = subject
       msg['From'] = sender
-      #msg['CC'] = 'lmendieta@terremark.com,stephen.brannon@verizon.com'
-      #msg['CC'] = 'lmendieta@terremark.com,stephen.brannon@verizon.com'
       msg['To'] = ", ".join(recipients)
-      #msg['To'] = 'lmendieta@verizon.com'	
-	
       s.sendmail(sender,recipients,msg.as_string())
       s.close()
       
@@ -331,7 +325,6 @@ class iocMinion():
 	maxpages = 0
 	caching = True
 	pagenos=set()
-        
         for page in  PDFPage.get_pages (fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
 	  interpreter.process_page(page)
 	  #print page
