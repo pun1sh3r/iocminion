d70219a5005ec09b5abba26f4966bbdfbf8e3c25
PuN1sh3r <luiguibiker@gmail.com>
2015-09-28 23:45:48 -0400

    adding output formats and fixing pdf parsing

--- iocminion.py
+++ iocminion.py
@@ -113,7 +113,7 @@ class iocMinion():
 
     def print_iocs(self, ioc_data, toFile, outFile, format):
         fileOut = []
-        #test
+        #testet
         today = datetime.date.today().strftime("%Y-%m-%d")
         for key, values in ioc_data.iteritems():
             key = key.replace('\n', '')
